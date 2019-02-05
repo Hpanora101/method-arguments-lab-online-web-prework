66719f0c4391d35ef57b66d72704d2ccd85d8ce8
Henry Panora <hpanora@gmail.com>
2019-02-05 01:08:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def intorduction_with_language(name, program)
-  puts "Hi, my name is #{name} and I am learning to program in in#{program}"
+  puts "Hi, my name is #{name} and I am learning to program in in #{program}."
 end
